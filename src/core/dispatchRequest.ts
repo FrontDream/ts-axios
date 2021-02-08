b3c860da98767f2022a9b4c4a8fee1ddf638102d
qiandingwei <qiandingwei@longfor.com>
2021-02-08 20:40:59 +0800

    feat:添加自定义参数序列化

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -24,8 +24,8 @@ function processConfig(config: AxiosRequestConfig) {
 }
 
 function transformUrl(config: AxiosRequestConfig) {
-  const { url, params } = config
-  return buildURL(url!, params)
+  const { url, params, paramsSerializer } = config
+  return buildURL(url!, params, paramsSerializer)
 }
 
 function transformResponseData(res: AxiosResponse) {
